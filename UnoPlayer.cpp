b7f8255d15a13111c23bf537095ab20bf1c032e8
doitmaan <alzaidy.abdullah@gmail.com>
2016-12-07 01:42:22 -0500

    Update UnoPlayer.cpp

--- UnoPlayer.cpp
+++ UnoPlayer.cpp
@@ -7,11 +7,13 @@ using namespace std;
 template<card card>
 class player {
 public:
-	player(string name, OrderedList<Card> hand) {
-		p_name =name;
-}
-	 card cardmatch()
-
+	player(string name, OrderedList<Card>* hand) {
+		p_name = name;
+		m_hand = hand;
+	}
+	auto cardmatch()
+		//need to pass the upface card ftom the deck (stack) 
+		//loop through the hand to check if there is a match 
 
 
 
@@ -19,9 +21,14 @@ public:
 private:
 
 	string p_name;
+	orderdList<card>* m_hand;
 
 
 
+	/*a name and a hand
+the hand is an orderedlist of cards or card pointers
+plus a match function
+that finds if the card passed to it is in their hand and if so returns it*/
 
 
 
